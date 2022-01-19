9c6b3dc32ba700d2cf4abfeef22579d9c2c5ba45
jocelin <queau.j@gmail.com>
2022-01-19 17:29:48 +0100

    workaround iterable

--- backend/src/xml/index.ts
+++ backend/src/xml/index.ts
@@ -40,49 +40,53 @@ const focus = [
 ];
 
 console.log(Object.keys(practiceTransfer).sort((a, b) => a.localeCompare(b)));
-
+function ensureIsArray(obj: unknown) {
+  return Array.isArray(obj) ? obj : [obj];
+}
 
 for (let foc in focus) {
-  const interestingData = _get(practiceTransfer, focus[foc][1], undefined);
-  console.log(interestingData);
+  const interestingData = ensureIsArray(_get(practiceTransfer, focus[foc][1], undefined));
+  console.log("..",interestingData);
   switch (focus[foc][0]) {
     case "accounts":
-      const accounts = interestingData.map(adapters.account) as [any];
+      const accounts = interestingData.map(adapters.account);
       console.log('here \n', accounts)
       console.log(typeof accounts)
+    
       console.log(Array.isArray(accounts))
       accounts.map((account) => {
         _once(() => console.log(account));
-        Account.create(...account);
+        Account.create({...account});
       });
+      
       break;
     case "banks":
       const banks = interestingData.map(adapters.bank);
-      banks.forEach((bank) => Bank.create(...bank));
+      banks.forEach((bank) => Bank.create({...bank}));
       break;
     case "contacts":
       const contacts = interestingData.map(adapters.contact);
-      banks.forEach((contact) => Contact.create(...contact));
+      contacts.forEach((contact) => Contact.create({...contact}));
       break;
     case "company":
       const companies = interestingData.map(adapters.bank);
-      companies.forEach((company) => Company.create(...company));
+      companies.forEach((company) => Company.create({...company}));
       break;
     case "exercices":
       const exercices = interestingData.map(adapters.financial_year);
-      exercices.forEach((fy) => Financial_Year.create(...fy));
+      exercices.forEach((fy) => Financial_Year.create({...fy}));
       break;
     case "immos":
       const immos = interestingData.map(adapters.capital_asset);
-      immos.forEach((immo) => Capital_Asset.create(...immo));
+      immos.forEach((immo) => Capital_Asset.create({...immo}));
       break;
     case "journals":
       const journals = interestingData.map(adapters.journal);
-      journals.forEach((journal) => Journal.create(...journal));
+      journals.forEach((journal) => Journal.create({...journal}));
       break;
     case "taxes":
       const taxes = interestingData.map(adapters.tax);
-      banks.forEach((tax_table) => Tax.create(...tax_table));
+      taxes.forEach((tax_table) => Tax.create({...tax_table}));
       break;
     default:
       _once(() => console.log(foc));
